7d15ad8baf64a826d6ca255b4be247c84862beb3
Waldemar Hummer <waldemar.hummer@gmail.com>
2024-05-22 19:17:49 +0200

    minor cleanup

--- tests/test_foo.py
+++ tests/test_foo.py
@@ -5,7 +5,7 @@ import requests
 from tests.config import API_URL
 
 
-def test_invoke_lambda():
+def test_invoke_lambda_via_api_gateway():
     result = requests.get(API_URL)
     payload = json.loads(result.content)
     assert payload["Hello"] == "World"
